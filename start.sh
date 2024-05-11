25bc782bcc8f725d314c4ebfccfbee5a3f07bf83
Sam P <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-05-12 00:17:36 +0100

    revert bugfix

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-export OPTIONS=$OPTIONS
+export OPTIONS="$OPTIONS"
 
 echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
 
 NEXT_CRON=$(crontab -l | grep -v '^#' | grep -m 1 -oE '[0-9]+ [0-9]+ [0-9]+ [0-9]+ [0-9]+' | awk '{$5+=1;print}' | tr ' ' '\t')
 
 NEXT_CRON_DATE=$(date -d"$NEXT_CRON" "+%M %H %d %m")
-HEALTH_CHECK_TIME=$(date -d "$NEXT_CRON_DATE +1 min" "+%M %H %d %m *")
+HEALTH_CHECK_TIME=$(date -d "$NEXT_CRON_DATE + 1 minute" "+%M %H %d %m")
 
 echo "$HEALTH_CHECK_TIME /healthcheck.sh" | crontab -
 
