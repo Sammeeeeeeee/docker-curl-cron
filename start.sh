754e8f6fe8900966ab35eef7408af07dee95c466
Sam P <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-05-11 22:58:22 +0100

    remove `now`

--- start.sh
+++ start.sh
@@ -4,9 +4,5 @@ set -e
 
 export OPTIONS=$OPTIONS
 
-if [[ "$1" == 'now' ]]; then
-    exec /curl.sh
-else
-	echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
-    crond -l 2 -f
-fi
+echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
+crond -l 2 -f
