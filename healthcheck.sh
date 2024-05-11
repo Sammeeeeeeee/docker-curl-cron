bbc5d329bcc95705c86fc78ab395f8c6592915ee
Sam P <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-05-11 23:43:05 +0100

    If `curl` fails, mark as unhealthy

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+if grep -q "CRON JOB FAILURE" /var/log/cron.log; then
+  exit 1  # Failure
+fi
+
 sleep 60
 
 if [ "$(find /var/log/curl.log -mmin -5 | wc -l)" -gt 0 ]; then
-  exit 0  
+  exit 0  # Success
 else
-  exit 1  
+  exit 1  # Failure
 fi
