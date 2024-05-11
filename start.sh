e898f7e82a7b22d998c4467d52eb3e03a382c345
Sam P <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-05-12 00:24:45 +0100

    simplify

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 set -e
 
 export OPTIONS="$OPTIONS"
@@ -9,8 +7,7 @@ echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
 NEXT_CRON=$(crontab -l | grep -v '^#' | grep -m 1 -oE '[0-9]+ [0-9]+ [0-9]+ [0-9]+ [0-9]+' | awk '{$5+=1;print}' | tr ' ' '\t')
 
 NEXT_CRON_DATE=$(date -d"$NEXT_CRON" "+%M %H %d %m")
-HEALTH_CHECK_TIME=$(date -d "$NEXT_CRON_DATE + 1 minute" "+%M %H %d %m")
 
-echo "$HEALTH_CHECK_TIME /healthcheck.sh" | crontab -
+(crontab -l ; echo "$(date -d "$NEXT_CRON_DATE + 1 minute" "+%M %H %d %m") /healthcheck.sh") | crontab -
 
 crond -l 2 -f
