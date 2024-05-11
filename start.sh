d6426b3c7b98c4a8f496e380067c1f4c7bd71960
Sam P <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-05-12 00:50:11 +0100

    Update start.sh  with no healthcheck

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
-## start here
 set -e
 
-export OPTIONS="$OPTIONS"
+export OPTIONS=$OPTIONS
 
-echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
-
-NEXT_CRON=$(crontab -l | grep -v '^#' | grep -m 1 -oE '[0-9]+ [0-9]+ [0-9]+ [0-9]+ [0-9]+' | awk '{$5+=1;print}' | tr ' ' '\t')
-
-NEXT_CRON_DATE=$(date -d"$NEXT_CRON" "+%M %H %d %m")
-
-(crontab -l ; echo "$(date -d "$NEXT_CRON_DATE + 1 minute" "+%M %H %d %m") /healthcheck.sh") | crontab -
+echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root#
 
 crond -l 2 -f
